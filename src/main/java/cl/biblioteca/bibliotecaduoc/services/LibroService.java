8b1571393c3d5a1b7764754b52ce9729cf60b394
Abrahamb93 <abraham.balvoap@gmail.com>
2026-03-27 01:12:42 -0300

    BibliotecaDuoc

---
+++ src/main/java/cl/biblioteca/bibliotecaduoc/services/LibroService.java
@@ -0,0 +1,35 @@
+package cl.biblioteca.bibliotecaduoc.services;
+
+import cl.biblioteca.bibliotecaduoc.model.Libro;
+import cl.biblioteca.bibliotecaduoc.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;;
+
+@Service
+public class LibroService {
+    @Autowired
+    private LibroRepository libroRepository;
+
+    public List<Libro> getLibros() {
+        return libroRepository.obtenerLibros();
+    }
+
+    public Libro saveLibro(Libro libro) {
+        return libroRepository.guardar(libro);
+    }
+
+    public Libro getLibroId(int id) {
+        return libroRepository.buscarPorId(id);
+    }
+
+    public Libro updateLibro(Libro libro) {
+        return libroRepository.actualizar(libro);
+    }
+
+    public String deleteLibro(int id) {
+        libroRepository.eliminar(id);
+        return "Producto eliminado";
+    }
+
+}
